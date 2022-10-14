15095c83b2b56ad84a0843e6eb58b0237538dfe3
root <bustlezach001@gmail.com>
2022-10-14 14:37:38 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet in reverse
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
