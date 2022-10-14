174a4d4ee51eb85a203bf28a083b75a135913577
root <bustlezach001@gmail.com>
2022-10-14 09:22:47 -0700

    task 1 and 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - using putchar
+ * Return: to produce lowercase of alphabet
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i = 'z'; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
