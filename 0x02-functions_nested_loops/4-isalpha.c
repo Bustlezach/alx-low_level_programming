768c0f88b5aa9398259bf118fc3c4ddc5fb97119
root <bustlezach001@gmail.com>
2022-10-18 17:38:21 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isalpha - check to confirm if its a letter
+ *
+ * Return: 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	char i, j;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			if ((c == i) || (c == j))
+			{
+				return (1);
+			}
+		}
+	}
+	return (0);
+}
