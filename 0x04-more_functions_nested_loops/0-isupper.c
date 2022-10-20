8de8201990771addb3aed50e4a082f436533db31
root <bustlezach001@gmail.com>
2022-10-20 05:52:41 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main"
+
+/**
+ * _isupper(int c) - check uppercase charater
+ *
+ * Return: 1 or 0
+ */
+
+int _isupper(int c);
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	return (0);
+}
